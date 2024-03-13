885c8bca998df62b754b3f236c70a40e7857d945
Morfinix <Morfinix@MORFPC>
2024-03-13 11:19:12 +0100

    Add project files.

---
+++ PWR-VI-PZ.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PWR_VI_PZ.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
